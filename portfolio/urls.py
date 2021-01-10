bd81d6c91d98b8fa588013c5408853df384b53e0
Joanna Nowakowska <joanna.k.nowakowska@gmail.com>
2021-01-10 15:19:38 +0100

    Home url added to urls.py and home.html created based on the Bootstrap code

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+import jobs.views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -21,4 +23,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', jobs.views.home, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
